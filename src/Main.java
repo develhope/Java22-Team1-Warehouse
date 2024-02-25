59a3fc08b8e4447d67d751823b2cc662453b7a6a
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-02-26 00:59:50 +0100

    messa creazione utente/operatore nei loro case

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Warehouse warehouse = new Warehouse();
+        warehouse.fillWarehouse();
         Cart cart = new Cart();
-        User user = new User();
-        Operatore operator = new Operatore();
         Scanner sc = new Scanner(System.in);
-
-        initializeMenu(sc, user, cart, warehouse, operator);
+        initializeMenu(sc, cart, warehouse);
     }
 
-    private static void initializeMenu(Scanner sc, User user, Cart cart, Warehouse warehouse, Operatore operator) {
+    private static void initializeMenu(Scanner sc, Cart cart, Warehouse warehouse) {
         System.out.println("1) Digitare 1 per profilo utente:");
         System.out.println("2) Digitare 2 per profilo operatore:");
+        System.out.println("0) Digitare 0 per uscire dal programma");
         String scelta = sc.next();
 
-        while (!scelta.equals("1") && !scelta.equals("2")) {
+        while (!scelta.equals("1") && !scelta.equals("2") && !scelta.equals("0")) {
 
-            if (!scelta.equals("1") && !scelta.equals("2")) {
+            if (!scelta.equals("1") && !scelta.equals("2") && !scelta.equals("0")) {
                 System.out.println("Scelta non disponiblie");
                 System.out.println("1) Digitare 1 per profilo utente:");
                 System.out.println("2) Digitare 2 per profilo operatore:");
+                System.out.println("0) Digitare 0 per uscire dal programma");
             }
             scelta = sc.next();
         }
         switch (scelta) {
             case "1":
+                User user = new User();
                 user.userMenu(cart, warehouse);
                 break;
             case "2":
+                Operatore operator = new Operatore();
                 operator.operatorMenu(warehouse);
+                break;
+            case "0":
                 break;
         }
     }
