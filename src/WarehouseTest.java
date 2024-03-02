7a742dd10a1d3b739de3bbe7a185c2eea020b928
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-03-02 13:05:44 +0100

    developed more Warehouse Tests

--- src/WarehouseTest.java
+++ src/WarehouseTest.java
@@ -1,9 +1,12 @@
 import Devices.DeviceClasses;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class WarehouseTest {
 
@@ -27,5 +30,49 @@ public class WarehouseTest {
 
         assertEquals(device, device2);
 
+    }
+
+    @Test
+    public void testRemoveDeviceById() {
+        Warehouse warehouse = new Warehouse();
+        DeviceClasses device = new DeviceClasses(1500, "Notebook", "Samsung", "Galaxy Book 3", "Gaming computer", 15.6, 1000, 799);
+        warehouse.addDevice(device);
+        warehouse.removeDeviceById(device.getId());
+
+        assertFalse(warehouse.containsDeviceById(device.getId()));
+    }
+
+    @Test
+    public void testContainsDeviceById() {
+        Warehouse warehouse = new Warehouse();
+        DeviceClasses device = new DeviceClasses(1500, "Notebook", "Samsung", "Galaxy Book 3", "Gaming computer", 15.6, 1000, 799);
+        warehouse.addDevice(device);
+
+        assertTrue(warehouse.containsDeviceById(device.getId()));
+    }
+
+    @Test
+    public void testPrintAllDevices() {
+        DeviceClasses device1 = new DeviceClasses(1500, "Notebook", "Samsung", "Galaxy Book 3", "Gaming computer", 15.6, 1000, 799);
+        DeviceClasses device2 = new DeviceClasses(979, "Smartphone", "Apple", "iPhone 15", "Nero", 6.1, 128, 349);
+        List<DeviceClasses> devices = new ArrayList<>();
+        devices.add(device1);
+        devices.add(device2);
+        Warehouse warehouse = new Warehouse();
+        warehouse.printAllDevices(true, true);
+
+        assertTrue("questi sono i miei device: " + devices, true);
+    }
+
+    @Test
+    public void testGetCompatibles() {
+        DeviceClasses device1 = new DeviceClasses(1500, "Notebook", "Samsung", "Galaxy Book 3", "Gaming computer", 15.6, 1000, 799);
+        DeviceClasses device2 = new DeviceClasses(979, "Smartphone", "Apple", "iPhone 15", "Nero", 6.1, 128, 349);
+        List<DeviceClasses> devices = new ArrayList<>();
+        devices.add(device1);
+        devices.add(device2);
+
+        assertEquals("Notebook", devices.get(0).getDevice());
+        assertEquals("Apple", devices.get(1).getBrand());
     }
 }
